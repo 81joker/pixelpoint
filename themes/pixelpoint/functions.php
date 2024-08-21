6688029fabbbb9fa5c3972ba7917d38bee8197d7
81joker <nehad.al.timimi@gmail.com>
2024-08-21 12:58:26 +0200

    Add files css + js + new changes

--- themes/pixelpoint/functions.php
+++ themes/pixelpoint/functions.php
@@ -1,23 +1,46 @@
-<?php
+<?php 
 
-require get_template_directory() . '/inc/customizer.php';
 
-function wpdevs_load_scripts(){
-    wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
-    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null );
-    wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', true );
+function pixelpoint_load_scripts(){
+
+    //   Google Font 
+    wp_enqueue_style( 'pixelpoint-google-fonts', '//fonts.googleapis.com/css?family=Work+Sans:400,500,600,700,800,900|Poppins:400,500,600,700&display=swap', array(), null );
+    wp_enqueue_style('pixelpoint-googleapiss', '//fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+
+
+    
+    //  Css Styles
+    wp_enqueue_style( 'pixelpoint-bootstrap-style',get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
+    wp_enqueue_style( 'pixelpoint-font-awesome-style',get_template_directory_uri() . '/assets/css/font-awesome.min.css' );
+    wp_enqueue_style( 'pixelpoint-elegant-icons',get_template_directory_uri() . '/assets/css/elegant-icons.css' );
+    // wp_enqueue_style( 'pixelpoint-template-style',get_template_directory_uri() . '/assets/css/owl.carousel.min.css' );
+    // wp_enqueue_style( 'pixelpoint-template-style',get_template_directory_uri() . '/assets/css/magnific-popup.css' );
+    // wp_enqueue_style( 'pixelpoint-template-style',get_template_directory_uri() . '/assets/css/slicknav.min.css' );
+    // wp_enqueue_style( 'pixelpoint-style-sass',get_template_directory_uri() . '/assets/sass/style.sass' );
+    wp_enqueue_style( 'pixelpoint-style-css',get_template_directory_uri() . '/assets/css/style.css' );
+
+
+
+    // JavaScript Libraries
+    // wp_enqueue_script('pixelpoint-ajax-js', '//ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('pixelpoint-jquery', get_theme_file_uri('/assets/js/jquery-3.3.1.min.js'), array('jquery'), '1.0', true);
+    wp_enqueue_script('pixelpoint-bootstrap-js', get_theme_file_uri('/assets/js/bootstrap.min.js'), array('jquery'), '1.0', true);
+    wp_enqueue_script('pixelpoint-magnific-popup-js', get_theme_file_uri('/assets/js/jquery.magnific-popup.min.js'), array('jquery'), '1.0', true);
+    wp_enqueue_script('pixelpoint-countdown-min-js', get_theme_file_uri('/assets/js/jquery.countdown.min.js'), array('jquery'), '1.0', true);
+    wp_enqueue_script('pixelpoint-slicknav', get_theme_file_uri('/assets/js/jquery.slicknav.js'), array('jquery'), '1.0', true);
+    wp_enqueue_script('pixelpoint-slicknav', get_theme_file_uri('/assets/js/jquery.slicknav.js'), array('jquery'), '1.0', true);
+    wp_enqueue_script('pixelpoint-owlcarousel-js', get_theme_file_uri('/assets/js/owl.carousel.min.js'), array('jquery'), '1.0', true);
+    wp_enqueue_script('pixelpoint-main-js', get_theme_file_uri('/assets/js/main.js'), array('jquery'), '1.0', true);
+
 }
-add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
+add_action( 'wp_enqueue_scripts', 'pixelpoint_load_scripts' );
 
-function wpdevs_config(){
-
-    $textdomain = 'wp-devs';
-    load_theme_textdomain( $textdomain, get_template_directory() . '/languages/' );
+function pixelpoint_config(){
 
     register_nav_menus(
         array(
-            'wp_devs_main_menu' => esc_html__( 'Main Menu', 'wp-devs' ),
-            'wp_devs_footer_menu' => esc_html__( 'Footer Menu', 'wp-devs' )
+            'wp_devs_main_menu' => esc_html__( 'Main Menu', 'pixelpoint' ),
+            'wp_devs_footer_menu' => esc_html__( 'Footer Menu', 'pixelpoint' )
         )
     );
 
@@ -37,63 +60,9 @@ function wpdevs_config(){
     add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ));
     add_theme_support( 'title-tag' );
 
-    add_theme_support( 'align-wide' );
-    add_theme_support( 'responsive-embeds' );
-    add_theme_support( 'editor-styles' );
-    add_editor_style( 'style-editor.css' );
+    // add_theme_support( 'align-wide' );
+    // add_theme_support( 'responsive-embeds' );
+    // add_theme_support( 'editor-styles' );
+    // add_editor_style( 'style-editor.css' );
 }
-add_action( 'after_setup_theme', 'wpdevs_config', 0 );
-
-add_action( 'widgets_init', 'wpdevs_sidebars' );
-function wpdevs_sidebars(){
-    register_sidebar(
-        array(
-            'name'  => esc_html__( 'Blog Sidebar', 'wp-devs' ),
-            'id'    => 'sidebar-blog',
-            'description'   => esc_html__( 'This is the Blog Sidebar. You can add your widgets here.', 'wp-devs' ),
-            'before_widget' => '<div class="widget-wrapper">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name'  => esc_html__( 'Service 1', 'wp-devs' ),
-            'id'    => 'services-1',
-            'description'   => esc_html__( 'First Service Area', 'wp-devs' ),
-            'before_widget' => '<div class="widget-wrapper">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name'  => esc_html__( 'Service 2', 'wp-devs' ),
-            'id'    => 'services-2',
-            'description'   => esc_html__( 'Second Service Area', 'wp-devs' ),
-            'before_widget' => '<div class="widget-wrapper">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name'  => esc_html__( 'Service 3', 'wp-devs' ),
-            'id'    => 'services-3',
-            'description'   => esc_html__( 'Third Service Area', 'wp-devs' ),
-            'before_widget' => '<div class="widget-wrapper">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'
-        )
-    );
-}
-
-if ( ! function_exists( 'wp_body_open' ) ){
-	function wp_body_open() {
-		do_action( 'wp_body_open' );
-	}
-}
+add_action( 'after_setup_theme', 'pixelpoint_config', 0 );
---
+++ themes/pixelpoint/parts/content-footer.php
@@ -0,0 +1,4 @@
+    <!-- Footer Section Begin -->
+     <hr>
+<h2>Footer</h2>
+    <!-- Footer Section End -->
